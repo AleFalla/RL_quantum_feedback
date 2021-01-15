066b86a5c94a5022c9e37e91e03e3b6650f68ebf
Alessio Fallani <fallani@matteo.rossi@unimi.it>
2021-01-15 11:06:16 +0100

    modifications

--- general/Matrices.py
+++ general/Matrices.py
@@ -4,20 +4,20 @@ import scipy
 
 
 def Optomech(params):
-    wm=params['wm'] #frequenza oscillatore meccanico presa unitaria
-    k=params['k']*wm #accoppiamento luce ambiente 
-    y=params['y']*wm #accoppiamento mech ambiente
-    eta=params['eta'] #efficienza misura su bagno luce
-    g=params['g']*wm #accoppiamento ML
+    wm=params['wm'] #mechanical oscillator frequency=1
+    k=params['k']*wm #light-env coupling
+    y=params['y']*wm #mirr-env coupling
+    eta=params['eta'] #measurement efficiency on light environment
+    g=params['g']*wm #mirror-light coupling
     detuning=params['detuning']*wm#1,-1,0
-    ne=params['ne'] #numero di fononi meccanici
-    na=params['na'] #numero fononi ottici
+    ne=params['ne'] #number of mirror phonons
+    na=params['na'] #number of light phonons
     phi=params['phi'] #LO phase
     
     cos=np.around(math.cos(phi),15)
     sin=np.around(math.sin(phi),15)
 
-    A=np.array([[-y/2,-wm,0,0],[wm,-y/2,g,0],[0,0,-k/2,detuning],[g,0,-detuning,-k/2]])#il detuning di hammerer è definito al contrario
+    A=np.array([[-y/2,-wm,0,0],[wm,-y/2,g,0],[0,0,-k/2,detuning],[g,0,-detuning,-k/2]]) 
     D=np.array([[(1+2*ne)*y,0,0,0],[0,(1+2*ne)*y,0,0],[0,0,(1+2*na)*k,0],[0,0,0,(1+2*na)*k]])
     eta=(eta/(1+2*na*eta))**0.5
     b=-(((k*eta)**0.5))
@@ -26,11 +26,11 @@ def Optomech(params):
 
     return A,D,B,E
 
-def Cavity(params): #questo è meno dettagliato dell'optomeccanica
+def Cavity(params): #this is less detailed than the optomechanical one
     
     k=params['k'] #loss rate
-    eta=params['eta'] #eta della misura
-    X=params['X_kunit']*k #accoppiamento hamiltoniana del sistema 
+    eta=params['eta'] #measurement efficiency
+    X=params['X_kunit']*k #hamiltonian coupling  
     a=-(X+k/2)
     b=X-k/2
     A=np.array([[a,0],[0,b]])
--- general/parameters_optomech.py
+++ general/parameters_optomech.py
@@ -5,11 +5,11 @@ cases={
     '0.5_r_0.3':{'wm':1,'k':0.5,'y':1.14e-4,'eta':1,'g':0.3,'detuning':-1,'ne':2,'na':0,'phi':0.72*math.pi},
     '4_h_0.3':{'wm':1,'k':4,'y':2e-7,'eta':1,'g':0.3,'detuning':-1,'ne':3.5e5,'na':0,'phi':0.6*math.pi},
     '4_r_0.3':{'wm':1,'k':4,'y':1.14e-4,'eta':1,'g':0.3,'detuning':-1,'ne':2,'na':0,'phi':0.63*math.pi},
-    '16_h_0.3':{'wm':1,'k':16,'y':2e-7,'eta':1,'g':0.3,'detuning':-1,'ne':3.5e5,'na':0,'phi':-0.48*math.pi},#questo fa schifo lo stesso
+    '16_h_0.3':{'wm':1,'k':16,'y':2e-7,'eta':1,'g':0.3,'detuning':-1,'ne':3.5e5,'na':0,'phi':-0.48*math.pi},
     '16_r_0.3':{'wm':1,'k':16,'y':1.14e-4,'eta':1,'g':0.3,'detuning':-1,'ne':2,'na':0,'phi':0.52*math.pi},
-    '0.5_h_0.9':{'wm':1,'k':0.5,'y':2e-7,'eta':1,'g':0.9,'detuning':-1,'ne':3.5e5,'na':0,'phi':-0.16*math.pi},#da qui sono g=0.9
+    '0.5_h_0.9':{'wm':1,'k':0.5,'y':2e-7,'eta':1,'g':0.9,'detuning':-1,'ne':3.5e5,'na':0,'phi':-0.16*math.pi},#from here g=0.9
     '0.5_r_0.9':{'wm':1,'k':0.5,'y':1.14e-4,'eta':1,'g':0.9,'detuning':-1,'ne':2,'na':0,'phi':0.12*math.pi},
-    '4_h_0.9':{'wm':1,'k':4,'y':2e-7,'eta':1,'g':0.9,'detuning':-1,'ne':3.5e5,'na':0,'phi':0.56*math.pi}, #questo va bene comunque
+    '4_h_0.9':{'wm':1,'k':4,'y':2e-7,'eta':1,'g':0.9,'detuning':-1,'ne':3.5e5,'na':0,'phi':0.56*math.pi}, 
     '4_r_0.9':{'wm':1,'k':4,'y':1.14e-4,'eta':1,'g':0.9,'detuning':-1,'ne':2,'na':0,'phi':-0.16*math.pi},
     '16_h_0.9':{'wm':1,'k':16,'y':2e-7,'eta':1,'g':0.9,'detuning':-1,'ne':3.5e5,'na':0,'phi':0.52*math.pi},
     '16_r_0.9':{'wm':1,'k':16,'y':1.14e-4,'eta':1,'g':0.9,'detuning':-1,'ne':2,'na':0,'phi':0.56*math.pi}
